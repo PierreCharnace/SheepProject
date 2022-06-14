b3e1d0f36753a7f59ddf9ab22b61b7b32ad184ee
PierreCharnace <pierre.charnace@gmail.com>
2022-06-14 15:58:24 +0200

    regis modif

--- front/store/userInfos/actions.js
+++ front/store/userInfos/actions.js
@@ -5,7 +5,6 @@ export default {
 
     createAccount: ({commit}, userInfos) => {
         axios.post('http://localhost:8000/api/auth/signup',{ 
-<<<<<<< HEAD
         email : userInfos.email,
         firstName : userInfos.firstName,
         lastName : userInfos.lastName,
@@ -15,22 +14,12 @@ export default {
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       }
-=======
-        email : 'email',
-        firstName : 'firstName',
-        lastName : 'latName',
-        password: 'password' 
->>>>>>> 4a2f111... yohou
     })
     .then(function(response) {
         console.log('15----->', response)
     })
     .catch(function (error) {
-<<<<<<< HEAD
         console.log('18----->', error);
-=======
-        console.log(error.response.data, userInfos);
->>>>>>> 4a2f111... yohou
     })
     }
 }
