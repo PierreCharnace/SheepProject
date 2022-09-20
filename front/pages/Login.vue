bd0fc0387f9b1936fa6b1115e3c090097bace259
PierreCharnace <pierre.charnace@gmail.com>
2022-09-20 17:54:21 +0200

    fix create process

--- front/pages/Login.vue
+++ front/pages/Login.vue
@@ -3,6 +3,7 @@
     <v-form ref="form" @submit.prevent="submit">
       <v-container fluid>
         <v-row>
+          <v-btn @click="cancel()">cancel</v-btn>
           <v-col cols="12" sm="6">
             <h1 v-if="mode == 'login'">Connection</h1>
             <h1 v-else>Inscription</h1>
@@ -19,29 +20,30 @@
               >
             </p>
             <v-text-field
-              v-model="lastName"
+              v-if="mode === 'create'"
+              v-model="object.lastName"
               color="purple darken-2"
               label=" Nom"
               required
-              v-if="mode === 'create'"
-            ></v-text-field>
+              :rules="[(v) => !!v || 'Nom incorrect']"
+            />
             <v-text-field
-              v-model="firstName"
+              v-if="mode === 'create'"
+              v-model="object.firstName"
               color="purple darken-2"
               label="Prénom"
               required
-              v-if="mode === 'create'"
+              :rules="[(v) => !!v || 'Prénom incorrect']"
             ></v-text-field>
             <v-text-field
-              v-model="email"
+              v-model="object.email"
               color="purple darken-2"
               label="Email"
               required
-            /> <!--<small v-if="!validateEmail">
-              "Email saisie incorrect"
-            </small>-->
+              :rules="[rules.email]"
+            />
             <v-text-field
-              v-model="firstPassword"
+              v-model="object.firstPassword"
               :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
               :type="show1 ? 'text' : 'password'"
               color="purple darken-2"
@@ -50,11 +52,12 @@
               max="8"
               min="4"
               required
+              :rules="[(v) => !!v || 'Le mot de passe n\'a pas le bon format.']"
               @click:append="show1 = !show1"
             />
             <v-text-field
               v-if="mode === 'create'"
-              v-model="passwordConfirm"
+              v-model="object.passwordConfirme"
               :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
               :type="show1 ? 'text' : 'password'"
               color="purple darken-2"
@@ -63,6 +66,7 @@
               max="8"
               min="4"
               required
+              :rules="[(v) => !!v || 'Mot de passe incorrect']"
               @click:append="show1 = !show1"
             />
             <small v-if="!validatePassword">
@@ -76,24 +80,14 @@
         <v-btn text> annuler </v-btn>
         <v-spacer></v-spacer>
         <v-btn
-          @click="createAccount()"
-          :class="{ disabled: !validatesFields }"
           v-if="mode == 'create'"
+          @click="createAccount()"
           text
           color="primary"
         >
           S'enregistrer
         </v-btn>
-        <v-btn
-          @click="login()"
-          type="submit"
-          :class="{ disabled: !validatesFields }"
-          v-else
-          text
-          color="primary"
-        >
-          Connection
-        </v-btn>
+        <v-btn v-else type="submit" text color="primary"> Connection </v-btn>
       </v-card-actions>
     </v-form>
 
@@ -106,87 +100,47 @@ export default {
   name: "login",
   data: function () {
     return {
+      object: this.initObject(),
       mode: "create",
-      email: "",
-      lastName: "",
-      firstName: "",
-      firstPassword: "",
-      passwordConfirm: "",
       show1: false,
+      rules: {
+        email: (v) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(v) || "Email non valide";
+        },
+      },
     };
   },
-
   mounted() {},
   computed: {
-    validatesFields: function () {
-      if (this.mode == "create") {
-        if (
-          this.email != "" &&
-          this.lastName != "" &&
-          this.firstName != "" &&
-          this.firstPassword != "" &&
-          this.passwordConfirm != "" &&
-          this.validatePassword == true
-        ) {
-          return true;
-        } else {
-          return false;
-        }
-      } else {
-        if (this.email != "" && this.firstPassword != "") {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    },
     validatePassword: function () {
-      if (this.passwordConfirm === this.firstPassword) {
-        return true;
-      } else {
-        return false;
-      }
+      if (this.passwordConfirme === this.firstPassword) return true;
+      return false;
     },
- 
   },
   methods: {
-    // create account or login mode
-    switchTocreateAccount: function () {
-      this.mode = "create";
-    },
-    switchToLogin: function () {
-      this.mode = "login";
-    },
-    login: function () {
-      this.$store.dispatch("userInfos/login", {
-        email: this.email,
-        password: this.passwordConfirm
-      }).then(function (response) {
-      }).catch(function (error) {
-        console.log(error);
-      });
+    cancel() {
+      this.$refs.form.resetValidation();
+      this.object = this.initObject();
     },
     createAccount: function () {
-      const self = this;
-      this.$store.dispatch("userInfos/createAccount", {
-        email: this.email,
-        lastName: this.lastName,
-        firstName: this.firstName,
-        password: this.passwordConfirm,
-        
-      }).then(function (response) {
-          //Ne pas oublier la modal!!!!!!!!!!!!!!!!!!
-          self.mode = "login"
-          console.log(response);
-      }).catch(function (error) {
-          console.log(error);
-      });
+      if (!this.$refs.form.validate()) return;
+      this.$store.dispatch("userInfos/createAccount", this.object);
+    },
+    initObject() {
+      return {
+        email: null,
+        lastName: "",
+        firstName: "",
+        firstPassword: "",
+        passwordConfirme: "",
+      };
     },
   },
-
   head() {
     return {
-      title:'Inscription',
+      title: "Inscription",
       titleTemplate: "",
       meta: [
         {
@@ -204,7 +158,6 @@ export default {
   color: grey !important;
   pointer-events: none;
 }
-
 .createAndLogin {
   color: #1976d2;
   text-decoration: underline;
@@ -213,17 +166,4 @@ export default {
 .createAndLogin:hover {
   background-color: rgba(255, 255, 255, 0.1);
 }
-
-header,
-h2 {
-  margin-bottom: 20px;
-}
-.modal-content {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
 </style>
