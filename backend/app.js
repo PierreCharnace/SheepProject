b93c670f176041bf81fd0c69afb880663a219fdf
PierreCharnace <pierre.charnace@gmail.com>
2022-12-08 10:36:38 +0100

    you

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config()
+//require('dotenv').config()
 
 //router
 const postRoutes = require('./routes/posts');
